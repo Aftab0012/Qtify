576983e0b2c59174ea51f4147fdf0919c598b54d
aftab yunus mulani <aftab.mulani1515@gmail.com>
2023-09-25 20:19:01 +0530

    changes

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <div>
-      <img src="/hero.png" className="w-screen h-[300px]" alt="hero image" />
+      <img src="/hero.png" className="w-screen" alt="hero image" />
     </div>
   );
 }
